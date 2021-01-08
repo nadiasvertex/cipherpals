0d843cbdf6ab44e81128b25b3c1ca324388f9791
Christopher Nelson <csnelson@bethel.jw.org>
2021-01-08 10:09:40 -0500

    Finish challenge 7

--- aes_decrypt.cpp
+++ aes_decrypt.cpp
@@ -1,8 +1,13 @@
-#include "sodium.h"
+#include <sodium.h>
+
+#include <openssl/conf.h>
+#include <openssl/err.h>
+#include <openssl/evp.h>
 
 #include <algorithm>
 #include <cassert>
 #include <iostream>
+#include <fstream>
 #include <string>
 #include <vector>
 
@@ -30,12 +35,13 @@ auto bin_to_hex(const std::vector<std::byte> &bin) -> std::string {
   return std::string(reinterpret_cast<std::string::value_type *>(hex.data()));
 }
 
-auto b64_to_bin(const std::string& b64) -> std::vector<std::byte> {
+auto b64_to_bin(const std::string &b64) -> std::vector<std::byte> {
   std::vector<std::byte> bin(size(b64));
   std::size_t bin_len = 0;
-  sodium_base642bin(reinterpret_cast<unsigned char *const>(bin.data()), bin.size(),
-		   reinterpret_cast<const char *const>(b64.data()), b64.size(),
-		   nullptr, &bin_len, nullptr, sodium_base64_VARIANT_ORIGINAL);
+  sodium_base642bin(reinterpret_cast<unsigned char *const>(bin.data()),
+                    bin.size(), reinterpret_cast<const char *const>(b64.data()),
+                    b64.size(), nullptr, &bin_len, nullptr,
+                    sodium_base64_VARIANT_ORIGINAL);
   bin.resize(bin_len);
   return bin;
 }
@@ -54,6 +60,51 @@ auto string_to_vector(const std::string &in) -> std::vector<std::byte> {
   return out;
 }
 
+auto aes_128_ecb_decrypt(const std::vector<std::byte> &cipher_text,
+                         const std::string &key, const std::string &iv)
+    -> std::string {
+  EVP_CIPHER_CTX *ctx;
+  int len = 0;
+  if (!(ctx = EVP_CIPHER_CTX_new())) {
+    ERR_print_errors_fp(stderr);
+    abort();
+  }
+
+  if (1 != EVP_DecryptInit(ctx, EVP_aes_128_ecb(),
+                           reinterpret_cast<const unsigned char *>(key.data()),
+                           reinterpret_cast<const unsigned char *>(iv.data()))) {
+    ERR_print_errors_fp(stderr);
+    abort();
+  }
+
+  std::string plain_text(1<<20, 0);
+  int plain_text_len = 0; 
+  if (1 != EVP_DecryptUpdate(ctx,
+                           reinterpret_cast<unsigned char *>(plain_text.data()),
+                           &len,
+                           reinterpret_cast<const unsigned char *>(cipher_text.data()),
+			   size(cipher_text))) {
+    ERR_print_errors_fp(stderr);
+    abort();
+  }
+  
+  plain_text_len = len;
+
+  if (1 != EVP_DecryptFinal(ctx,
+                           reinterpret_cast<unsigned char *>(plain_text.data()) + plain_text_len,
+                           &len)) {
+    ERR_print_errors_fp(stderr);
+    abort();
+  }
+ 
+  plain_text_len += len;
+
+  EVP_CIPHER_CTX_free(ctx);
+ 
+  plain_text.resize(plain_text_len);
+  return plain_text;
+}
+
 int main(int argc, const char *argv[]) {
   if (argc < 2) {
     std::cerr << "usage: aes_decrypt <key>\n";
@@ -62,14 +113,28 @@ int main(int argc, const char *argv[]) {
 
   std::string key(argv[1]);
   std::string b64_text;
-
-  auto kv = string_to_vector(key);
-  for (std::string line; std::getline(std::cin, line);) {
+  
+  if (argc == 3) {
+	  std::ifstream in(argv[2]);
+	  
+  for (std::string line; std::getline(in, line);) {
     b64_text += line;
   }
 
-  auto cipher_bin = b64_to_bin(b64_text);
+   
+  } else {
+  for (std::string line; std::getline(std::cin, line);) {
+    b64_text += line;
+  }
+  }
+  
 
+  auto kv = string_to_vector(key);
+
+  auto cipher_bin = b64_to_bin(b64_text);
+  auto plain_text = aes_128_ecb_decrypt(cipher_bin, key, key);
+
+  std::cout << plain_text << std::endl;
 
   return 0;
 }
---
+++ b.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+clang++-11 -g -std=c++20 -stdlib=libc++ -lsodium -o hex_to_b64 hex_to_b64.cpp
+clang++-11 -g -std=c++20 -stdlib=libc++ -lsodium -o xor_str xor_str.cpp
+clang++-11 -g -std=c++20 -stdlib=libc++ -lsodium -o xor_crack_cipher xor_crack_cipher.cpp
+clang++-11 -g -std=c++20 -stdlib=libc++ -lsodium -o xor_encipher_rk xor_encipher_rk.cpp
+clang++-11 -g -std=c++20 -stdlib=libc++ -lsodium -lcrypto -o aes_decrypt aes_decrypt.cpp
+
